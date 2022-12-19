ea194accc68563bde51ab61776287d9d59e2d958
Gal Cohen <gala0@post.bgu.ac.il>
2022-12-19 21:52:56 +0200

    return to x

--- src/main/java/bguspl/set/ex/Player.java
+++ src/main/java/bguspl/set/ex/Player.java
@@ -1,5 +1,19 @@
 package bguspl.set.ex;
 
+import java.io.Console;
+import java.nio.file.ClosedWatchServiceException;
+import java.security.spec.EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+
+import javax.swing.UIDefaults.ProxyLazyValue;
+
+import org.w3c.dom.events.Event;
+
 import bguspl.set.Env;
 
 /**
@@ -41,7 +55,7 @@ public class Player implements Runnable {
     private final boolean human;
 
     /**
-     * True iff game should be terminated.
+     * True iff game should be terminated due to an external event.
      */
     private volatile boolean terminate;
 
@@ -49,6 +63,19 @@ public class Player implements Runnable {
      * The current score of the player.
      */
     private int score;
+
+    private Queue<Integer> tokQueue;
+
+    private int flag;
+    
+    public Object locObject;
+
+    private volatile boolean freeze;
+
+    private volatile boolean needToFreeze;
+    
+
+    
 
     /**
      * The class constructor.
@@ -64,6 +91,11 @@ public class Player implements Runnable {
         this.table = table;
         this.id = id;
         this.human = human;
+
+        tokQueue=  new ConcurrentLinkedQueue<Integer>();
+        freeze = true;
+        needToFreeze = false;
+        locObject = new Object();
     }
 
     /**
@@ -74,12 +106,40 @@ public class Player implements Runnable {
         playerThread = Thread.currentThread();
         env.logger.info("Thread " + Thread.currentThread().getName() + " starting.");
         if (!human) createArtificialIntelligence();
-
         while (!terminate) {
             // TODO implement main player loop
+            synchronized(this){
+                try {wait();} 
+                catch (InterruptedException ignored) {}
+            }
+            pointOrPenalty();
+
         }
         if (!human) try { aiThread.join(); } catch (InterruptedException ignored) {}
         env.logger.info("Thread " + Thread.currentThread().getName() + " terminated.");
+    }
+
+    public void removeNotActiveTokens()
+    {
+        List<Integer> keep = new ArrayList<>();
+        int size = tokQueue.size();
+
+        for (int i = 0; i < size; i++) {
+            Integer temp = tokQueue.poll();
+            if(table.playerContainsToken(temp, id))
+                keep.add(temp);    
+        }
+
+        for (int i = 0; i < keep.size(); i++) {
+            tokQueue.add(keep.get(i));
+        }
+                freeze = false;
+        synchronized(locObject){
+            
+                needToFreeze=false;
+                locObject.notifyAll();
+            
+        }
     }
 
     /**
@@ -87,14 +147,25 @@ public class Player implements Runnable {
      * key presses. If the queue of key presses is full, the thread waits until it is not full.
      */
     private void createArtificialIntelligence() {
-        // note: this is a very, very smart AI (!)
+        // note: this is a very very smart AI (!)
         aiThread = new Thread(() -> {
             env.logger.info("Thread " + Thread.currentThread().getName() + " starting.");
+            Random rnd = new Random();
             while (!terminate) {
                 // TODO implement player key press simulator
-                try {
-                    synchronized (this) { wait(); }
-                } catch (InterruptedException ignored) {}
+
+                synchronized(locObject){
+                    keyPressed(rnd.nextInt(env.config.tableSize));//
+                }
+                    
+                    if(needToFreeze){
+                        try {
+                            synchronized(locObject){
+                            {
+                                locObject.wait();}
+                            }
+                        } catch (InterruptedException ignored) {}}
+                        
             }
             env.logger.info("Thread " + Thread.currentThread().getName() + " terminated.");
         }, "computer-" + id);
@@ -102,10 +173,19 @@ public class Player implements Runnable {
     }
 
     /**
-     * Called when the game should be terminated.
+     * Called when the game should be terminated due to an external event.
      */
     public void terminate() {
         // TODO implement
+        terminate = true;
+        synchronized(locObject)
+        {
+            locObject.notifyAll();
+        }
+        synchronized(this)
+        {
+            notifyAll();
+        }
     }
 
     /**
@@ -115,6 +195,62 @@ public class Player implements Runnable {
      */
     public void keyPressed(int slot) {
         // TODO implement
+        
+        
+        if(!table.isInShuflle & !freeze)
+        {
+            
+            if(tokQueue.contains(slot) & table.isCard(slot))
+            {
+                table.removeToken(id, slot);
+                tokQueue.remove(slot);
+            }
+            else if(table.isCard(slot) && tokQueue.size()<Table.tokenToSet)
+            {
+                tokQueue.add(slot);
+
+                if(tokQueue.size()==Table.tokenToSet)
+                    {
+                        freeze= true;
+                        needToFreeze = true;
+                        table.pushPid(id);
+                    }
+
+                table.placeToken(id, slot);
+                
+            }
+            
+        }
+        
+            
+    }
+
+    public void setFlag(int num)
+    {
+        flag = num;
+        synchronized(this)
+        {
+            notifyAll();
+        }
+    }
+
+    public void pointOrPenalty ()
+    {
+        
+        if(flag == Table.goodSet)
+            {
+                point();
+                flag = Table.neutralFlag;
+                
+            }
+        else if(flag == Table.badSet)
+            {
+                penalty();
+                flag = Table.neutralFlag;
+                
+            }
+        
+        
     }
 
     /**
@@ -123,21 +259,91 @@ public class Player implements Runnable {
      * @post - the player's score is increased by 1.
      * @post - the player's score is updated in the ui.
      */
-    public void point() {
+    public void point() { /////need sync
         // TODO implement
+        synchronized(locObject){
+            tokQueue.clear();
+            int ignored = table.countCards(); // this part is just for demonstration in the unit tests
+            env.ui.setScore(id, ++score);
+            env.ui.setFreeze(id, env.config.pointFreezeMillis);
+            // freeze = true;
+            try {
+                Thread.sleep(env.config.pointFreezeMillis);
+            } catch (InterruptedException ign) {}
+            freeze = false;
+            env.ui.setFreeze(id, Table.resetFreeze);
 
-        int ignored = table.countCards(); // this part is just for demonstration in the unit tests
-        env.ui.setScore(id, ++score);
+            needToFreeze = false;
+            locObject.notifyAll();
+        }
+        
+            
     }
 
     /**
      * Penalize a player and perform other related actions.
      */
-    public void penalty() {
+    public void penalty() { ///need sync
         // TODO implement
+        synchronized(locObject){
+        int count = 0 ;
+        
+        while(count<env.config.penaltyFreezeMillis/1000)
+        {
+            env.ui.setFreeze(id, env.config.penaltyFreezeMillis - count * Table.oneSec);
+            
+            try {
+                Thread.sleep(Table.oneSec);
+            } catch (InterruptedException ignored) {}
+            count++;
+        } 
+        freeze = false; 
+        env.ui.setFreeze(id, Table.resetFreeze);
+        removeNotActiveTokens();
+        }
+        
     }
 
     public int score() {
         return score;
     }
+
+    public void removeAllTokens()
+    {
+        tokQueue.clear();
+    }
+
+    public boolean getFreeze()
+    {
+        return freeze;
+    }
+
+    
+    public void freeze()
+    {
+        // synchronized(locObject){
+        needToFreeze = true;
+        freeze = true;
+        // }
+    }
+
+    public void unfreeze()
+    {
+        synchronized(locObject)
+        {
+            needToFreeze = false;
+            freeze = false;
+            tokQueue.clear();
+            locObject.notifyAll();
+        }
+    }
+
+
+    //###################FOR_TESTS
+
+    public int get_QueueSize()
+    {
+        return tokQueue.size();
+    }
+    
 }
